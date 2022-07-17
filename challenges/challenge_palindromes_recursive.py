f00547f709e701b5dab7d4ae4cd4ad8e5842cf8e
Bruno Trindade <bruno@DESKTOP-I1C99NP.localdomain>
2022-07-17 15:03:12 -0300

    feat: palindrome recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if (len(word) < 2):
+    if (len(word) < 1):
         return False
+    return word == reverse(word)
+
+def reverse(word):
+    if (len(word) <= 1):
+        return word
+    return word[-1] + reverse(word[:len(word) - 1])

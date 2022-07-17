4544a28b4ef28114dcc7bbcc7f1d3894319ad3be
Bruno Trindade <bruno@DESKTOP-I1C99NP.localdomain>
2022-07-17 18:41:50 -0300

    fix: flake8 adjust

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,8 +3,14 @@ def study_schedule(permanence_period, target_time):
         return None
     total_students = 0
     for index in range(0, len(permanence_period)):
-        if (type(permanence_period[index][0]) is not int or type(permanence_period[index][1]) is not int):
+        if (
+            type(permanence_period[index][0]) is not int
+            or type(permanence_period[index][1]) is not int
+        ):
             return None
-        if (target_time >= permanence_period[index][0] and target_time <= permanence_period[index][1]):
+        if (
+            target_time >= permanence_period[index][0]
+            and target_time <= permanence_period[index][1]
+        ):
             total_students += 1
     return total_students

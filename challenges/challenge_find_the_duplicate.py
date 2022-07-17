e74e6352c160972ce427b8604b3205012894fa2b
Bruno Trindade <bruno@DESKTOP-I1C99NP.localdomain>
2022-07-17 17:48:31 -0300

    feat: find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,11 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if (nums is None or len(nums) < 2):
+        return False
+    nums.sort()
+    for index, num in enumerate(nums[1:], start=1):
+        if (type(num) is not int or num < 0):
+            return False
+        if (nums[index - 1] == nums[index]):
+            return num
+
+    return False

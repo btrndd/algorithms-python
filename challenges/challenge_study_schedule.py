5f4d7d1daa1bc93749f4e100377da2cfd3c6bb57
Bruno Trindade <bruno@DESKTOP-I1C99NP.localdomain>
2022-07-17 14:42:53 -0300

    fix: flake adjusts

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,5 +1,5 @@
 def study_schedule(permanence_period, target_time):
-    if (target_time is None or target_time < 0):
+    if (target_time is None):
         return None
     total_students = 0
     for student in permanence_period:
@@ -7,6 +7,6 @@ def study_schedule(permanence_period, target_time):
             return None
         if (type(student[0]) is not int or type(student[1]) is not int):
             return None
-        if (target_time >= student[0] and target_time <= student [1]):
+        if (target_time >= student[0] and target_time <= student[1]):
             total_students += 1
     return total_students

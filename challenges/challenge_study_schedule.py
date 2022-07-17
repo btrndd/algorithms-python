b1737cbe69aa1ad4cfa790286fd3f0adcedd968e
Bruno Trindade <bruno@DESKTOP-I1C99NP.localdomain>
2022-07-17 18:38:26 -0300

    fix: time in study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,9 +2,9 @@ def study_schedule(permanence_period, target_time):
     if (target_time is None):
         return None
     total_students = 0
-    for student in permanence_period:
-        if (type(student[0]) is not int or type(student[1]) is not int):
+    for index in range(0, len(permanence_period)):
+        if (type(permanence_period[index][0]) is not int or type(permanence_period[index][1]) is not int):
             return None
-        if (target_time >= student[0] and target_time <= student[1]):
+        if (target_time >= permanence_period[index][0] and target_time <= permanence_period[index][1]):
             total_students += 1
     return total_students

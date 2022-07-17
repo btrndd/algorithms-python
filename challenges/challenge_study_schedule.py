d92903e088a50895adea3c25dfd15e0883512655
Bruno Trindade <bruno@DESKTOP-I1C99NP.localdomain>
2022-07-17 14:35:09 -0300

    feat: study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if (target_time is None or target_time < 0):
+        return None
+    total_students = 0
+    for student in permanence_period:
+        if (student[0] is None or student[1] is None):
+            return None
+        if (type(student[0]) is not int or type(student[1]) is not int):
+            return None
+        if (target_time >= student[0] and target_time <= student [1]):
+            total_students += 1
+    return total_students
